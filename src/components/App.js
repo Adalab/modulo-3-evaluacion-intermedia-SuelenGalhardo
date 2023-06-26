dbfa8cb3df3fd53e1c5b479b43cd2f58f02a82ec
SuelenGalhardo <suelenranucci@gmail.com>
2023-06-26 12:18:29 +0200

    filterpricipio

--- src/components/App.js
+++ src/components/App.js
@@ -5,29 +5,42 @@ import { useEffect } from 'react';
 
 function App() {
   const [contactList, setContactList] = useState([]);
-  const [nameSearch, setNameSearch] = useState('');
+  const [filterQuote, setFilterQuote] = useState('');
+  const [filterCharacter, setFilterChatacter] = useState('all');
 
   useEffect(() => {
     fetch(
       'https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json'
     )
       .then((response) => response.json())
-      .then((data) => {
-        setContactList(data);
+      .then((dataApi) => {
+        setContactList(dataApi);
       });
   }, []);
 
-  // const handleSeach
+  const handleCharacter = (ev) => {
+    setFilterChatacter(ev.target.value);
+  };
 
+  const handleQuote = (ev) => {
+    setFilterQuote(ev.target.value);
+  };
+
+  // const handleSeach
+  //para pintar una lista, hay que hacer un map, simpre, henderizar
+  //filter simpre antes del map,
   const renderContactlist = () => {
-    return contactList.map((eachQuote, index) => (
-      <li key={index}>
-        <p>
-          {eachQuote.quote}
-          <span>{eachQuote.character}</span>
-        </p>
-      </li>
-    ));
+    return contactList
+      .filter((element) => element.quote.toLowerCase.incluides(filterQuote.toLowerCase()))
+      .filter(element) => {
+        
+      }
+      .map((element, index) => (
+        <li key={index}>
+          <h4>{element.quote}</h4>
+          <span>{element.character}</span>
+        </li>
+      ));
   };
 
   return (
@@ -38,15 +51,17 @@ function App() {
       <main>
         <form action=''>
           <label htmlFor=''>Filtrar por frase:</label>
-          <input type='text' value={nameSearch} onInput={handleSeach} />
+          <input type='text' value={filterQuote} onInput={handleQuote} />
 
           <label htmlFor='personaje'>Filtrar por Personaje</label>
-          <select name='personaje' id=''>
-            <option value=''>todos</option>
+          <select name='personaje' id='' onChange={handleCharacter}>
+            <option value='all'>todos</option>
             <option value='Joey'>Joey</option>
             <option value='Phoebe'>Phoebe</option>
             <option value='Rachel'>Rachel</option>
             <option value='Ross'>Ross</option>
+            <option value='Monica'>Monica</option>
+            <option value='Chandler'>Chandler</option>
           </select>
         </form>
         <ul>{renderContactlist()}</ul>
