4c63a009d0208389b213d099b3d509e3d6fc52ae
SuelenGalhardo <suelenranucci@gmail.com>
2023-06-26 10:47:37 +0200

    fechtlis

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,34 @@
 // react , archivos proprios, sass, imagens
 
 import '../styles/App.scss';
-import data from '../data/data.json';
-//import { useState } from 'react';
+//import data from '../data/data.json';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
-  // const [contactList, setContactList] = useState(data);
+  const [contactList, setContactList] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      'https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setContactList(data);
+      });
+  }, []);
+
+  const renderContactlist = () => {
+    return contactList.map((eachQuote, index) => (
+      <li key={index}>
+        <p>
+          {eachQuote.quote}
+          <span>{eachQuote.character}</span>
+        </p>
+      </li>
+    ));
+  };
+
   return (
     <div>
       <header>
@@ -26,7 +49,7 @@ function App() {
             <option value='Ross'>Ross</option>
           </select>
         </form>
-        <ul></ul>
+        <ul>{renderContactlist}</ul>
       </main>
     </div>
   );
