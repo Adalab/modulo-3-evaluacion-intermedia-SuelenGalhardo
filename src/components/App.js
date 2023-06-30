3c20344ee5f26f13cc844dc1119d8d47ce467c2e
SuelenGalhardo <suelenranucci@gmail.com>
2023-06-30 12:54:23 +0200

    added css

--- src/components/App.js
+++ src/components/App.js
@@ -61,15 +61,13 @@ function App() {
         })
         //import data from '../data/data.json';
         .map((element, index) => (
-          <li key={index}>
+          <li className='main__li' key={index}>
             <h4>{element.quote}</h4>
             <span>{element.character}</span>
           </li>
         ))
     );
   };
-
-  //  .map(  (element, index)    =>  {  return <li }      )
 
   return (
     <div>
@@ -79,69 +77,74 @@ function App() {
       </header>
 
       <main className='main'>
-        <form className='main__form' action=''>
-          <label className='main__label' htmlFor='filter'>
-            Filtrar por frase:
-          </label>
-          <input
-            className='main__input'
-            type='text'
-            id='filter'
-            value={filterQuote}
-            onInput={handleQuote}
-          />
+        <section className='main__section'>
+          <form className='main__form' action=''>
+            <label className='main__label' htmlFor='filter'>
+              Filtrar por frase:
+            </label>
+            <input
+              className='main__input'
+              type='text'
+              id='filter'
+              value={filterQuote}
+              onInput={handleQuote}
+            />
 
-          <label className='main__label' htmlFor='personajeF'>
-            Filtrar por Personaje
-          </label>
-          <select
-            className='main__select'
-            name='personaje'
-            id='personajeF'
-            onChange={handleCharacter}
-          >
-            <option value='all'>todos</option>
-            <option value='Joey'>Joey</option>
-            <option value='Phoebe'>Phoebe</option>
-            <option value='Rachel'>Rachel</option>
-            <option value='Ross'>Ross</option>
-            <option value='Monica'>Monica</option>
-            <option value='Chandler'>Chandler</option>
-          </select>
-        </form>
-        <ul className='main__list'>{renderContactlist()}</ul>
-
-        <form className='formTwo' action=''>
-          <label className='formTwo__label' htmlFor='frase'>
-            {' '}
-            Añade la Frase
-          </label>
-          <input
-            className='formTwo__input'
-            type='text'
-            id='frase'
-            name='quote'
-            onInput={handleNewQuote}
-            value={newQuote.quote}
-          />
-          <label className='formTwo__label' htmlFor='personaje'>
-            Añade el Personaje
-          </label>
-          <input
-            className='formTwo__input'
-            type='text'
-            id='personaje'
-            name='character'
-            onInput={handleNewQuote}
-            value={newQuote.character}
-          />
-          <button className='formTwo__button' onClick={handleClick}>
-            Añadir la nueva frase
-          </button>
-        </form>
+            <label className='main__label' htmlFor='personajeF'>
+              Filtrar por Personaje
+            </label>
+            <select
+              className='main__select'
+              name='personaje'
+              id='personajeF'
+              onChange={handleCharacter}
+            >
+              <option value='all'>todos</option>
+              <option value='Joey'>Joey</option>
+              <option value='Phoebe'>Phoebe</option>
+              <option value='Rachel'>Rachel</option>
+              <option value='Ross'>Ross</option>
+              <option value='Monica'>Monica</option>
+              <option value='Chandler'>Chandler</option>
+            </select>
+          </form>
+        </section>
+        <section className='main__section'>
+          <ul className='main__list'>{renderContactlist()}</ul>
+        </section>
+        <section className='main__section'>
+          <form className='' action=''>
+            <label className='main__label' htmlFor='frase'>
+              {' '}
+              Añade la Frase
+            </label>
+            <input
+              className='main__input'
+              type='text'
+              id='frase'
+              name='quote'
+              onInput={handleNewQuote}
+              value={newQuote.quote}
+            />
+            <label className='main__label' htmlFor='personaje'>
+              Añade el Personaje
+            </label>
+            <input
+              className='main__input'
+              type='text'
+              id='personaje'
+              name='character'
+              onInput={handleNewQuote}
+              value={newQuote.character}
+            />
+            <button className='main__button' onClick={handleClick}>
+              Añadir la nueva frase
+            </button>
+          </form>
+        </section>
       </main>
       <footer className='footer'>
-        <p className='footer__text'>Adalab 2023 © Friens</p>
+        <p className='footer__text'>Adalab 2023 © Friends</p>
       </footer>
     </div>
   );
